a3968373c0d2c173981f8a15ce88b3335ecb36fb
Marian Oancea <contact@siteshop.ro>
2015-01-30 23:37:38 +0200

    more filters

--- public/js/filters.js
+++ public/js/filters.js
@@ -3,6 +3,17 @@
 /* Filters */
 
 angular.module('netStatsApp.filters', [])
+.filter('mainClass', function() {
+  return function(node, bestBlock) {
+  	if( ! node.active)
+  		return 'text-danger';
+
+  	if( node.peers === 0)
+  		return 'text-danger';
+
+    return (node.peers <= 1 ? 'text-danger' : (node.peers > 1 && node.peers < 4 ? 'text-warning' : 'text-success'));
+  }
+})
 .filter('peerClass', function() {
   return function(peers) {
     return (peers <= 1 ? 'text-danger' : (peers > 1 && peers < 4 ? 'text-warning' : 'text-success'));
