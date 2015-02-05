81ce05ca1a460caa7d9cd12c9a669a7c7e37a01e
Marian Oancea <contact@siteshop.ro>
2015-02-05 08:30:58 +0200

    added back test nodes to config/nodes.default.js

--- config/nodes.default.js
+++ config/nodes.default.js
@@ -11,6 +11,24 @@ var nodes = [
 	// 	rpcPort: 'JSON-RPC port here'
 	// },
 	{
+		name: 'instance 1',
+		type: 'C++',
+		rpcHost: '54.173.252.182',
+		rpcPort: '8080'
+	},
+	{
+		name: 'instance 2',
+		type: 'C++',
+		rpcHost: '54.209.133.248',
+		rpcPort: '8080'
+	},
+	{
+		name: 'instance 3',
+		type: 'C++',
+		rpcHost: '54.174.67.126',
+		rpcPort: '8080'
+	},
+	{
 		name: 'local',
 		type: 'C++',
 		rpcHost: 'localhost',
