0ff0ce9d6785a9119b90775b9d517d25c29646bc
Marian Oancea <contact@siteshop.ro>
2015-02-05 12:08:06 +0200

    added moment customised locale

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,7 @@
 'use strict';
 
 var app = angular.module('netStatsApp', ['underscore', 'angularMoment', 'netStatsApp.filters', 'netStatsApp.directives']);
+
+app.run(function(amMoment) {
+	amMoment.changeLocale('en-gb');
+});
