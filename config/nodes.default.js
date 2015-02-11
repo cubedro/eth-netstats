41ad5413a066b148038d9f9b8557f120c186dfc4
Marian Oancea <contact@siteshop.ro>
2015-02-11 22:50:44 +0200

    updated nodes

--- config/nodes.default.js
+++ config/nodes.default.js
@@ -32,10 +32,73 @@ var nodes = [
 		rpcPort: '8080'
 	},
 	{
-		name: 'local',
+		name: 'instance 4',
 		type: 'C++',
-		os: 'OS X',
-		rpcHost: 'localhost',
+		os: 'linux',
+		rpcHost: '54.152.111.192',
+		rpcPort: '8080'
+	},
+	{
+		name: 'instance 5',
+		type: 'C++',
+		os: 'linux',
+		rpcHost: '52.0.57.108',
+		rpcPort: '8080'
+	},
+	{
+		name: 'instance 6',
+		type: 'C++',
+		os: 'linux',
+		rpcHost: '52.0.28.243',
+		rpcPort: '8080'
+	},
+	{
+		name: 'instance 7',
+		type: 'C++',
+		os: 'linux',
+		rpcHost: '52.1.21.22',
+		rpcPort: '8080'
+	},
+	{
+		name: 'instance 8',
+		type: 'C++',
+		os: 'linux',
+		rpcHost: '52.0.207.16',
+		rpcPort: '8080'
+	},
+	{
+		name: 'instance 9',
+		type: 'C++',
+		os: 'linux',
+		rpcHost: '52.0.250.60',
+		rpcPort: '8080'
+	},
+	{
+		name: 'instance 10',
+		type: 'C++',
+		os: 'linux',
+		rpcHost: '52.0.160.249',
+		rpcPort: '8080'
+	},
+	{
+		name: 'instance 11',
+		type: 'C++',
+		os: 'linux',
+		rpcHost: '52.0.216.64',
+		rpcPort: '8080'
+	},
+	{
+		name: 'instance 12',
+		type: 'C++',
+		os: 'linux',
+		rpcHost: '52.0.144.162',
+		rpcPort: '8080'
+	},
+	{
+		name: 'instance 13',
+		type: 'C++',
+		os: 'linux',
+		rpcHost: '52.0.93.126',
 		rpcPort: '8080'
 	}
 ];
