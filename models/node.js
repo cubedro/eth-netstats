a01706c9715d2dab00dbd6008862a824e886663e
cubedro <contact@siteshop.ro>
2015-03-27 14:22:28 +0200

    fixed block.number

--- models/node.js
+++ models/node.js
@@ -89,7 +89,7 @@ Node.prototype.setStats = function(stats)
 {
 	if(typeof stats !== 'undefined' && typeof stats.block !== 'undefined' && typeof stats.block.number !== 'undefined')
 	{
-		if(stats.block.number !== this.stats.number) {
+		if(stats.block.number !== this.stats.block.number) {
 			stats.block.received = (new Date()).getTime() - stats.block.arrival;
 		} else {
 			stats.block.received = this.stats.block.received;
