ffcb50e196b43d95dd65aa0eeb16f5b5f58ffde8
cubedro <contact@siteshop.ro>
2015-06-10 05:04:17 +0300

    removed express from app.js

--- app.js
+++ app.js
@@ -18,46 +18,7 @@ var env = 'production';
 
 if( process.env.NODE_ENV !== 'production' )
 {
-	var express = require('express');
-	var app = express();
-	var path = require('path');
-	var bodyParser = require('body-parser');
-
-	// view engine setup
-	app.set('views', path.join(__dirname, 'src/views'));
-	app.set('view engine', 'jade');
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({ extended: false }));
-	app.use(express.static(path.join(__dirname, 'dist')));
-
-	app.get('/', function(req, res) {
-	  res.render('index');
-	});
-
-	// catch 404 and forward to error handler
-	app.use(function(req, res, next) {
-		var err = new Error('Not Found');
-		err.status = 404;
-		next(err);
-	});
-
-	// error handlers
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
-
-	// production error handler
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: {}
-		});
-	});
+	var app = require('./lib/express');
 
 	server = require('http').createServer(app);
 }
