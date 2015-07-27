d4e2f3a6cabb0dc2bf999db4789a88dfda4edd4e
cubedro <contact@siteshop.ro>
2015-07-27 15:02:38 +0300

    added grunt tasks for dist-lite

---
+++ src-lite/js/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/* Init Angular App */
+
+var netStatsApp = angular.module('netStatsApp', ['netStatsApp.filters', 'netStatsApp.directives', 'ngStorage']);
+
+
+/* Services */
+
+netStatsApp.factory('socket', function ($rootScope) {
+	var socket = new Primus();
+	return socket;
+});
+
+netStatsApp.factory('toastr', function ($rootScope) {
+	toastr = window.toastr;
+	toastr.options = {
+		"closeButton": false,
+		"debug": false,
+		"progressBar": false,
+		"newestOnTop": true,
+		"positionClass": "toast-top-right",
+		"preventDuplicates": false,
+		"onclick": null,
+		"showDuration": "300",
+		"hideDuration": "1000",
+		"timeOut": "5000",
+		"extendedTimeOut": "1000",
+		"showEasing": "swing",
+		"hideEasing": "linear",
+		"showMethod": "fadeIn",
+		"hideMethod": "fadeOut"
+	};
+	return toastr;
+});
+
+netStatsApp.factory('_', function ($rootScope) {
+	var lodash = window._;
+	return lodash;
+});
