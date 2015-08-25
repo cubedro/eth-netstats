8d50891c76106ceb02a02878d88c485d44293b55
cubedro <contact@siteshop.ro>
2015-08-25 12:32:33 +0300

    updated block time colors

--- src/js/script.js
+++ src/js/script.js
@@ -13,10 +13,10 @@
 	$.fn.sparkline.defaults.bar.tooltipFormat = $.spformat('<div class="tooltip-arrow"></div><div class="tooltip-inner">{{prefix}}{{value}} {{suffix}}</div>');
 	$.fn.sparkline.defaults.bar.colorMap = $.range_map({
 		'0:6': '#10a0de',
-		'6:13': '#7bcc3a',
-		'13:20': '#FFD162',
-		'20:30': '#ff8a00',
-		'30:': '#F74B4B'
+		'6:15': '#7bcc3a',
+		'15:40': '#FFD162',
+		'40:60': '#ff8a00',
+		'60:': '#F74B4B'
 	});
 
 	moment.relativeTimeThreshold('s', 60);
