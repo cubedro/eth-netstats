fa365e40e53f896105204d20959043f4c559e406
Jonathan Brown <jbrown@mix-blockchain.org>
2018-05-03 15:56:43 +0700

    Fix it for China.

--- Gruntfile.js
+++ Gruntfile.js
@@ -106,7 +106,7 @@ module.exports = function(grunt) {
 					{
 						expand: true,
 						cwd: 'src/fonts/',
-						src: ['minimal-*.*'],
+						src: ['*.*'],
 						dest: 'dist/fonts/',
 						filter: 'isFile'
 					},
