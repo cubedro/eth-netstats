45a79922e13b03638ef5085bcfd11d0b481cc417
EOS Classic <info@eos-classic.io>
2018-09-16 01:42:52 +0900

    Remove un-maintained files

--- src-lite/js/directives.js
+++
@@ -1,8 +0,0 @@
-/* Directives */
-
-angular.module('netStatsApp.directives', [])
-	.directive('appVersion', ['version', function (version) {
-		return function(scope, elm, attrs) {
-			elm.text(version);
-		};
-}]);
